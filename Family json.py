f1b0a0fa56221e43da65bbd29dc415c30af1fb9f
Ewan Colley <21ec11@alderchs.uk>
2025-07-07 14:38:46 +0100

    Printed First Name for each Family member

--- Family json.py
+++ Family json.py
@@ -1,8 +1,7 @@
 import json
 
 with open('FamilyInformation.json', 'r') as file:
-    fileContent = file.read()
-    fileDataJSON = json.loads(fileContent)
+    fileDataJSON = json.load(file)
 
 for person in fileDataJSON["38DanbyRoad"]:
     print(person['FirstName'])
