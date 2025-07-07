d42ce5fb29e5a429bf4ba1e6ba828459da44cd24
Ewan Colley <21ec11@alderchs.uk>
2025-07-07 15:44:35 +0100

    Finished rows for 38DanbyRoad

--- Family json.py
+++ Family json.py
@@ -5,8 +5,8 @@ with open('FamilyInformation.json', 'r') as file:
 
 familyTable = []
 for person in fileDataJSON["38DanbyRoad"]:
-    familyTable.append([person["FirstName"], person["LastName"], person["Age"], (person["Height"]["Feet"], person["Height"]["Inches"])])
-
+    familyTable.append([person["FirstName"], person["LastName"], person["Age"], (person["Height"]["Feet"], person["Height"]["Inches"]), person["HairColour"]])
+    familyTable.append("\n")
 
 print(familyTable)
 
