8a60c983bd496148a0065e5de657c3999836a93e
Ewan Colley <21ec11@alderchs.uk>
2025-07-08 14:51:58 +0100

    Finished full program with Unique Identifiers

--- Family json.py
+++ Family json.py
@@ -9,23 +9,20 @@ UqId = 0
 i = 0
 HeightUqId = 0
 heightList = []
-
-def HeightUnique(uniqueID):
-    foot1=4
-    inch1=0
-    j=0
-    for i in range(24):
-        heightList = []
-        j+=1
-        if j==uniqueID:
-            heightList.append(j)
-            #When back from lunch, investigate error with printing ALL heights.
-            return
-        inch1+=1
-        if inch1 >=13:
-            inch1==0
-            foot1+=1
-
+HeightUqId = 0
+newHeightTable = []
+foot = 4
+inch = 0
+for i in range(27):
+    HeightUqId+=1
+    if inch >=12:
+        inch-=12
+        foot+=1
+    heightF = str(foot)
+    heightI = str(inch)
+    print(HeightUqId,", ", heightF, heightI)
+    inch+=1
+print("")
 
 for person in danbyRoad:
     peopleList = []
@@ -34,10 +31,22 @@ for person in danbyRoad:
          peopleList.append(str(UqId))
     for key, value in person.items():
         if isinstance(value, dict) and key == "Height":
-            feet = value.get("Feet", 0)
-            inches = value.get("Inches", 0)
-            peopleList.append(f"{feet}")
-            peopleList.append(f"{inches}")
+            footNew = str(value.get("Feet", 0))
+            inchNew = str(value.get("Inches", 0))
+            HeightUqId = 0
+            newHeightTable = []
+            foot = 4
+            inch = 0
+            for i in range(27):
+                HeightUqId+=1
+                if inch >=12:
+                    inch-=12
+                    foot+=1
+                heightF = str(foot)
+                heightI = str(inch)
+                inch+=1
+                if footNew == heightF and inchNew == heightI:
+                    peopleList.append(str(HeightUqId))
         else:
             peopleList.append(str(value))
     print(", ".join(peopleList))
@@ -49,31 +58,25 @@ for member in cheritonClose:
          newList.append(str(UqId))
     for key, value in member.items():
         if isinstance(value, dict) and key == "Height":
-            newList.append(str(value.get("Feet", 0)))
-            newList.append(str(value.get("Inches", 0)))
+            footNew = str(value.get("Feet", 0))
+            inchNew = str(value.get("Inches", 0))
+            HeightUqId = 0
+            newHeightTable = []
+            foot = 4
+            inch = 0
+            for i in range(27):
+                HeightUqId+=1
+                if inch >=12:
+                    inch-=12
+                    foot+=1
+                heightF = str(foot)
+                heightI = str(inch)
+                inch+=1
+                if footNew == heightF and inchNew == heightI:
+                    newList.append(str(HeightUqId))
         else:
             newList.append(str(value))
     print(", ".join(newList))
 print("")
 
-for height in danbyRoad:
-    HeightUqId+=1
-    if i == 0:
-        heightList.append(str(HeightUqId))
-    for key, value in height.items():
-        if isinstance(value, dict) and key == "Height":
-            heightList.append(str(value.get("Feet", 0)))
-            heightList.append(str(value.get("Inches", 0)))
-    print(", ".join(heightList))
-
-for height in cheritonClose:
-    HeightUqId+=1
-    if i == 0:
-        heightList.append(str(HeightUqId))
-    for key, value in height.items():
-        if isinstance(value, dict) and key == "Height":
-            heightList.append(str(value.get("Feet", 0)))
-            heightList.append(str(value.get("Inches", 0)))
-    print(", ".join(heightList))
-
-HeightUnique(UqId)
+    
