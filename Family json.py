f27e4352d8c1fe25148328659d95f356d3a90f40
Ewan Colley <21ec11@alderchs.uk>
2025-07-08 11:20:59 +0100

    Experimenting with objects inside objects

--- Family json.py
+++ Family json.py
@@ -2,17 +2,16 @@ import json
 
 with open('FamilyInformation.json', 'r') as file:
     fileDataJSON = json.load(file)
-keyList = ["FirstName","LastName","Age","Height","HairColour"]
-familyTable = []
+keyList = ["'FirstName'","'LastName'","'Age'","'Height'","'HairColour'"]
 filterTable = fileDataJSON["38DanbyRoad"]
-i=0
-for person in filterTable:
-    familyTable.append(filterTable[i])
-    familyTable.append("\n")
-    i+=1
-    printable = filterTable[0]
-    #Fix it jumping from one object and key. Should only jump one key.
-print(familyTable)
+
+for i in filterTable:
+    valuesList = []
+    items = i.values()
+    for j in items: 
+        newList = valuesList.append(j)
+        print(j)
+    print(str(newList).strip("None"))
 
 #for person in fileDataJSON["38DanbyRoad"]:
 #    print(person)
