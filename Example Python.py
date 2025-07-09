42432f5c81dbe1922ca5b5df6a6a027f6309510a
Derek Colley <derek.colley@global-technologists.co.uk>
2025-07-09 15:45:28 +0100

    Universal .json printer with PUqId's and UqId's for each item.

--- Example Python.py
+++ Example Python.py
@@ -4,55 +4,61 @@ filename = input("Enter file name... ")
 with open(filename, 'r') as file:
     fileDataJSON = json.load(file)
 
-level_list = []
-max_level = 1
+uqid_counter = 1
+structured_output = []
 
-def collect_by_level(obj, level=1, results=None):
-    if results is None:
-        results = {}
+def assign_uqid():
+    global uqid_counter
+    uqid = uqid_counter
+    uqid_counter += 1
+    return uqid
 
+def flatten(obj, top_level_uqid, level):
     if isinstance(obj, dict):
-        for v in obj.values():
-            collect_by_level(v, level + 1, results)
+        for key, value in obj.items():
+            key_uqid = assign_uqid()
+            structured_output.append({
+                "UqID": key_uqid,
+                "PUqID": top_level_uqid,
+                "Type": "Key",
+                "Level": level,
+                "Value": key
+            })
+            flatten(value, top_level_uqid, level + 1)
     elif isinstance(obj, list):
-        for item in obj:
-            collect_by_level(item, level, results)
+        for i, item in enumerate(obj):
+            index_uqid = assign_uqid()
+            structured_output.append({
+                "UqID": index_uqid,
+                "PUqID": top_level_uqid,
+                "Type": "Key",
+                "Level": level,
+                "Value": f"[{i}]"
+            })
+            flatten(item, top_level_uqid, level + 1)
     else:
-        if level not in results:
-            results[level] = []
-        results[level].append(str(obj))
-    
-    return results
+        value_uqid = assign_uqid()
+        structured_output.append({
+            "UqID": value_uqid,
+            "PUqID": top_level_uqid,
+            "Type": "Value",
+            "Level": level,
+            "Value": str(obj)
+        })
 
-root_items = []
+for top_key, people in fileDataJSON.items():
+    top_uqid = assign_uqid()
+    structured_output.append({
+        "UqID": top_uqid,
+        "PUqID": 0,
+        "Type": "Key",
+        "Level": 1,
+        "Value": top_key
+    })
+    for person in people:
+        flatten(person, top_uqid, level=2)
 
-if isinstance(fileDataJSON, dict):
-    if all(isinstance(v, list) for v in fileDataJSON.values()):
-        for sublist in fileDataJSON.values():
-            root_items.extend(sublist)
-    else:
-        root_items = [fileDataJSON]
-
-elif isinstance(fileDataJSON, list):
-    root_items = fileDataJSON
-
-else:
-    raise TypeError("Unsupported JSON root format")
-
-for item in root_items:
-    values_by_level = collect_by_level(item, level=1)
-    level_list.append(values_by_level)
-    max_level = max(max_level, max((k for k in values_by_level if isinstance(k, int)), default=1))
-
-for level in range(2, max_level + 1):
-    lines = [
-        ", ".join(str(v) for v in levels[level])
-        for levels in level_list if level in levels
-    ]
-    if lines:
-        print(f"Information (Level {level - 1}):")
-        for line in lines:
-            print(line)
-        print("")
-
+print(f"{'UqID':<6} {'PUqID':<6} {'Level':<5} {'Type':<6} Value")
+for entry in structured_output:
+    print(f"{entry['UqID']:<6} {entry['PUqID']:<6} {entry['Level']:<5} {entry['Type']:<6} {entry['Value']}")
 file.close()
